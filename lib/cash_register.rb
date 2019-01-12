8254bf37487a83ad3968b9c766fa450e4e8472d2
Chris Kakos <ckakos@gmail.com>
2019-01-12 17:51:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,9 +14,12 @@ class CashRegister
     # amount * quantity
     # self.total += amount
     self.total += amount * quantity
+    quanitity.times do
+      self.items << title
+    end
   end
 
-  def apply_discount
+  def apply_discount(total_price)
     self.discount -= self.total
   end
 
